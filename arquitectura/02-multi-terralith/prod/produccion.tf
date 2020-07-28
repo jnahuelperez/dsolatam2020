25382f47b31b2f904b49d253dbba03040cf154e8
Nahuel Perez <jnahuelperez@hotmail.com>
2020-07-28 20:59:57 -0300

    whoopppsi

--- arquitectura/02-multi-terralith/prod/produccion.tf
+++ arquitectura/02-multi-terralith/prod/produccion.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "splunk-prod" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["8.8.8.8/32"]
   }
 
   egress {
